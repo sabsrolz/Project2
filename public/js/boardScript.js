642e8c57b777effbe957aaa0417449d087f18281
Sean Lim <lim_s@outlook.com>
2019-11-05 23:24:15 -0600

    started a leaderboard (maybe attach to home page?)

---
+++ public/js/boardScript.js
@@ -0,0 +1,6 @@
+$(".sidenav").sidenav();
+
+// To do:
+
+// Get data of users and total of asset values (probably on routing side)
+// Display on table, and sort by net worth (https://blog.niklasottosson.com/javascript/jquery-sort-table-rows-on-column-value/)
