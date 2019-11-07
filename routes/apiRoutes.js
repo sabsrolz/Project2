9d0c2f8698c96876daeb20e84f991ccffde2f10e
sabsrolz <sabsrolz@umich.edu>
2019-11-07 14:47:31 -0600

    added PUT route to updated user funds post transaction

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,9 +10,9 @@ module.exports = function(app) {
     let num_shares;
     let total_price = 0;
     let currentTime = moment()
-    .add(1, "hours")
-    .subtract(1, "minutes")
-    .format("YYYY-MM-DD HH:mm:00");
+      .add(1, "hours")
+      .subtract(1, "minutes")
+      .format("YYYY-MM-DD HH:mm:00");
     console.log(currentTime);
     function searchTicker(company) {
       const api_key = "8HGF9L0ALM5LPNX5"; //send to env
@@ -36,9 +36,8 @@ module.exports = function(app) {
     }
   });
 
-  
   //POST ROUTE when user purchases/sells # of shares at $ price
-  app.post("/api/transaction/user", function(req, res) {
+  app.post("/api/transaction/:user", function(req, res) {
     //req.body = {numShares, buy/sell}
     const numShares = req.body.numShares;
     const transactionType = req.body.transactionType;
@@ -46,92 +45,91 @@ module.exports = function(app) {
     const companyName = req.body.companyName;
     const ticker = req.body.ticker;
     const userId = req.params.user;
-    transTotal = numShares*currentPrice;
-    const updatedFunds;
+    transTotal = numShares * currentPrice;
     let transaction;
-    db.user.findAll({
-      where: {
-        user: userId
-      }
-    }).then(function(result, err) {
-      if (err) throw err;
-      // res.json(result);
-      // console.log(result);
-    const currentFunds = result.fundsAvailable;
-    if (transactionType === "buy"){
-      if (currentFunds >= transTotal){
-        //call put api to update current funds of user
-        transaction = {
-          companyName: companyName,
-          ticker: ticker,
-          userId: userId,
-          sharesTraded: numShares,
-          transactionPrice: transTotal
-        }
-        db.transactions.create({
-          transaction
-        }).then(function(err, result){
-          if (err) throw (err);
-          console.log(result)
-          console.log("transaction was successfully recorded")
-        })
-      }else{
-        console.log("you dont have enough funds to complete transaction")
-      }
-    }
-    else { 
-      db.transactions_table.findAndCountAll({
-        include: [{ticker:ticker}], 
-        where:{
-          userId: userId
-        }
-      }).then(function(err, result){
-        if (err) throw err;
-        console.log(result)
-        if (numShares >= result.count){
-          transaction = {
-            companyName: companyName,
-            ticker: ticker,
-            userId: userId,
-            sharesTraded: numShares,
-            transactionPrice: transTotal
-          }
-          //call put api to update current funds of user
-          db.transactions.create({
-            transaction
-          }).then(function(err, result){
-            if (err) throw (err);
-            console.log(result)
-            console.log("transaction was successfully recorded")
-          })
-        }
-        else {
-          console.log("Insufficient shares to complete transaction")
+    let updatedFunds;
+    db.user
+      .findAll({
+        where: {
+          user: userId
         }
       })
-    }
-    
+      .then(function(result, err) {
+        if (err) throw err;
+        // res.json(result);
+        // console.log(result);
+        const currentFunds = result.fundsAvailable;
+        if (transactionType === "buy") {
+          if (currentFunds >= transTotal) {
+            updatedFunds = currentFunds - transTotal;
+            updateUser(updatedFunds);
+            transaction = {
+              companyName: companyName,
+              ticker: ticker,
+              userId: userId,
+              sharesTraded: numShares,
+              transactionPrice: transTotal
+            };
+            db.transactions
+              .create({
+                transaction
+              })
+              .then(function(err, result) {
+                if (err) throw err;
+                console.log(result);
+                console.log("transaction was successfully recorded");
+              });
+          } else {
+            console.log("you dont have enough funds to complete transaction");
+          }
+        } else {
+          db.transactions_table
+            .findAndCountAll({
+              include: [{ ticker: ticker }],
+              where: {
+                userId: userId
+              }
+            })
+            .then(function(err, result) {
+              if (err) throw err;
+              console.log(result);
+              if (numShares >= result.count) {
+                updatedFunds = currentFunds + transTotal;
+                transaction = {
+                  companyName: companyName,
+                  ticker: ticker,
+                  userId: userId,
+                  sharesTraded: numShares,
+                  transactionPrice: transTotal
+                };
+                updatedFunds(updatedFunds);
+                db.transactions
+                  .create({
+                    transaction
+                  })
+                  .then(function(err, result) {
+                    if (err) throw err;
+                    console.log(result);
+                    console.log("transaction was successfully recorded");
+                  });
+              } else {
+                console.log("Insufficient shares to complete transaction");
+              }
+            });
+        }
+      });
   });
-   
 
-    //if sell: 
-    //get from transactions count of ticker selected
-    //if amount is met, then update funds available PUT + 
-    //insert a new transaction into transactions table
-
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  //put calls
+  function updateUser(x) {
+    app.put("/api/transaction/:user", function(req, res) {
+      db.user
+        .updateOne({ fundsAvailable: x }, { userId: req.params.user })
+        .then(function(data, err) {
+          if (err) throw err;
+          console.log(data);
+          res.json(data);
+        });
     });
-  });
-
-  //write a PUT route and wrap inside function
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
+  }
 };
