643e77433fce81589b82fd36af9e5b8ff50bb14a
Sean Lim <lim_s@outlook.com>
2019-11-09 12:23:40 -0600

    unsure why committing again

--- server.js
+++ server.js
@@ -41,24 +41,24 @@ if (process.env.NODE_ENV === "test") {
 
 // Socket IO connection
 // var app = require("express")(); // we don't need this line
-// var server = require("http").Server(app);
-// var io = require("socket.io")(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-// server.listen(80);
-// // WARNING: app.listen(80) will NOT work here!
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
 
-// app.get("/", function(req, res) {
-//   res.sendFile(__dirname + "/public/alpha_ts.html");
-// });
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/alpha_ts.html");
+});
 
-// io.on("connection", function(socket) {
-//   socket.emit("news", {
-//     hello: "world"
-//   });
-//   socket.on("my other event", function(data) {
-//     console.log(data);
-//   });
-// });
+io.on("connection", function(socket) {
+  socket.emit("news", {
+    hello: "world"
+  });
+  socket.on("my other event", function(data) {
+    console.log(data);
+  });
+});
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
