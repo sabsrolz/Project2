96afd6c51a739371ca97690979738eaf95b9b7ef
Sean Lim <lim_s@outlook.com>
2019-11-10 08:19:51 -0600

    un-functioned functions that only run once

--- public/js/boardScript.js
+++ public/js/boardScript.js
@@ -2,41 +2,35 @@ $(".sidenav").sidenav();
 
 // To do:
 
-// Get data of users and total of asset values (probably on routing side)
+// Get data of users and total of asset values (probably on routing side - nope i lied let's do it here - Sean)
 
-// Display on table, and sort by net worth (https://blog.niklasottosson.com/javascript/jquery-sort-table-rows-on-column-value/)
+$.get();
 
-function sortTable() {
-  const rows = $("#leaderboard tbody tr").get();
-  rows.sort(function(first, second) {
-    let A = $(first)
-      .children("td")
-      .eq(3)
-      .text()
-      .toUpperCase();
-    let B = $(second)
-      .children("td")
-      .eq(3)
-      .text()
-      .toUpperCase();
-    return B - A;
-  });
-  rows.forEach(row => {
-    $("#leaderboard")
-      .children("tbody")
-      .append(row);
-  });
-}
-sortTable();
+const rows = $("#leaderboard tbody tr").get();
+rows.sort(function(first, second) {
+  let A = $(first)
+    .children("td")
+    .eq(3)
+    .text()
+    .toUpperCase();
+  let B = $(second)
+    .children("td")
+    .eq(3)
+    .text()
+    .toUpperCase();
+  return B - A;
+});
+rows.forEach(row => {
+  $("#leaderboard")
+    .children("tbody")
+    .append(row);
+});
 
-function rankings() {
-  const rows = $("#leaderboard tbody tr").get();
-  console.log(rows);
-  rows.forEach(row => {
-    $(row)
-      .children()
-      .eq(0)
-      .text(row.rowIndex);
-  });
-}
-rankings();
+const rowranks = $("#leaderboard tbody tr").get();
+console.log(rowranks);
+rowranks.forEach(row => {
+  $(row)
+    .children()
+    .eq(0)
+    .text(row.rowIndex);
+});
