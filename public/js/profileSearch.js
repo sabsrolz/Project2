279fe4d28b6b3694e0f0b144bc0c508a4a600b5c
Sean Lim <lim_s@outlook.com>
2019-11-10 13:16:55 -0600

    pepehands

--- public/js/profileSearch.js
+++ public/js/profileSearch.js
@@ -1,3 +1,12 @@
 $(".sidenav").sidenav();
 
-// $("#")
+$("form").on("submit", function() {
+  // search user by name to get id
+  // findall transactions by id for portfolio
+  // api all stocks owned by user and total with funds for net worth
+
+  // display on page
+
+  event.preventDefault();
+  $("#profileInfo").removeClass("hide");
+});
