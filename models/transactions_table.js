7dce0359fb405e57a4e38f860f1d6b88bf632f59
Sean Lim <lim_s@outlook.com>
2019-11-10 06:25:26 -0600

    timestamps: false for troubleshooting

--- models/transactions_table.js
+++ models/transactions_table.js
@@ -1,10 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
-  var Transactions = sequelize.define("Transactions", {
-    companyName: DataTypes.STRING,
-    ticker: DataTypes.STRING,
-    userId: DataTypes.STRING,
-    sharesTraded: DataTypes.INTEGER,
-    transactionPrice: DataTypes.DECIMAL
-  });
+  var Transactions = sequelize.define(
+    "Transactions",
+    {
+      companyName: DataTypes.STRING,
+      ticker: DataTypes.STRING,
+      userId: DataTypes.STRING,
+      sharesTraded: DataTypes.INTEGER,
+      transactionPrice: DataTypes.DECIMAL
+    },
+    { timestamps: false }
+  );
   return Transactions;
 };
