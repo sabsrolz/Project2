d82b95bfe8b134366052148de4318778bdd8133d
Sean Lim <lim_s@outlook.com>
2019-11-11 20:25:43 -0600

    leaderboard gets all data and populates on table

--- public/js/boardScript.js
+++ public/js/boardScript.js
@@ -6,17 +6,15 @@ $(".sidenav").sidenav();
 
 $.get("api/allUsers", function(userData) {
   const usersArray = [];
+
   const stockData = [];
 
+  // console.log(usersArray);
   for (let i = 0; i < userData.length; i++) {
     usersArray.push(userData[i]);
   }
 
-  console.log(usersArray);
-
   $.get("/api/allUserTransactions", function(transactions) {
-    console.log(transactions);
-
     for (let i = 0; i < transactions.length; i++) {
       if (!stockData.includes(transactions[i].companyName)) {
         stockData.push({
@@ -39,12 +37,70 @@ $.get("api/allUsers", function(userData) {
         stockData[i].currentPrice = liveData.currentStockPrice;
         itemsProcessed++;
         if (itemsProcessed === stockData.length) {
-          console.log(stockData);
-          for (let i = 0; i < usersArray.length; i++) {
+          // console.log(stockData);
+          for (let j = 0; j < usersArray.length; j++) {
             //  I DON'T KNOW HOW TO DO THIS WITHOUT FOR-IN BUT IT'S ASYNC
+
+            for (const stock in usersArray[j].portfolio) {
+              // console.log(stock);
+              for (
+                let stockIndex = 0;
+                stockIndex < stockData.length;
+                stockIndex++
+              ) {
+                if (stockData[stockIndex].companyName === stock) {
+                  // console.log(stock + stockData[stockIndex].companyName);
+
+                  let newTotal =
+                    parseFloat(usersArray[j].netWorth) +
+                    parseFloat(
+                      stockData[stockIndex].currentPrice *
+                        usersArray[j].portfolio[stock]
+                    );
+
+                  usersArray[j].netWorth = newTotal;
+                }
+              }
+            }
             // usersArray.portfolio is an object, we want
             // foreach in portfolio usersArray.netWorth += portfolio.ELEMENT * stockData[THIS IS AN ARRAY  ヽ(  ⁰Д⁰)ﾉ ]
           }
+          usersArray.forEach(element => {
+            const newRow = $(
+              `<tr><td>${0}</td><td>${element.firstName} ${
+                element.lastName
+              }</td><td>${element.funds}</td><td>${element.netWorth}</td></tr>`
+            );
+            $("#tbody").append(newRow);
+          });
+
+          const rows = $("#leaderboard tbody tr").get();
+          rows.sort(function(first, second) {
+            let A = $(first)
+              .children("td")
+              .eq(3)
+              .text()
+              .toUpperCase();
+            let B = $(second)
+              .children("td")
+              .eq(3)
+              .text()
+              .toUpperCase();
+            return B - A;
+          });
+          rows.forEach(row => {
+            $("#leaderboard")
+              .children("tbody")
+              .append(row);
+          });
+
+          const rowranks = $("#leaderboard tbody tr").get();
+          rowranks.forEach(row => {
+            $(row)
+              .children()
+              .eq(0)
+              .text(row.rowIndex);
+          });
         }
       });
     }
